79dcf90bd06f04be232d4d58e3d5830bbc6bb73e
Said Oudouane <saidoudouane@gmail.com>
2022-06-11 20:41:43 +0100

    add responsive article view page

--- frontend/pages/articles/[article].tsx
+++ frontend/pages/articles/[article].tsx
@@ -1,34 +1,61 @@
 import React from "react";
 import styles from "@modules/Article.module.scss";
-import { externalArticleData } from "@utils/data";
+import { articleData } from "@utils/data";
+import Link from "next/link";
 import { format } from "date-fns";
 
 const Article = () => {
-  const { excerpt, media, published_date, summary, title } =
-    externalArticleData;
+  const {
+    author,
+    content,
+    created_at,
+    id,
+    preview,
+    section,
+    summary,
+    tags,
+    title,
+  } = articleData;
   return (
     <div className={styles.article}>
-      <div className={styles.poster}>
-        <img src={media} alt={title} />
+      <div className={styles.intro}>
+        <Link href={`/sections/${section.id}`}>
+          <a className={styles.section}>{section.name}</a>
+        </Link>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.summary}>{summary}</p>
+        <div className={styles.author}>
+          <div className={styles.avatar}>
+            {author.avatar ? (
+              <img src={author.avatar} alt={author.name} />
+            ) : (
+              author.name
+                .split(" ")
+                .map((v) => v[0])
+                .join("")
+            )}
+          </div>
+          <p className={styles.name}>
+            By{" "}
+            <Link href={`/contributors/${author.id}`}>
+              <a className={styles.link}>{author.name}</a>
+            </Link>
+          </p>
+        </div>
       </div>
       <div className={styles.main}>
-        <div className={styles.intro}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.excerpt}>{excerpt}</p>
+        <div className={styles.content}>
+          <div className={styles.info}>
+            <span className={styles.date}>
+              {format(new Date(created_at), "LLLL d, yyyy H:mmaaa")}
+            </span>
+            <div className={styles.socials}>socials</div>
+          </div>
+          <div className={styles.preview}>
+            <img src={preview} alt={title} />
+          </div>
         </div>
-        <div className={styles.info}>
-          <p className={styles.publish}>
-            {format(new Date(published_date), "MMM dd, yyyy, h:m aaa")}
-          </p>
-          <div className={styles.socials}>socials</div>
-        </div>
-        <div
-          className={styles.summary}
-          dangerouslySetInnerHTML={{ __html: summary }}
-        />
-        <a href="#" className={styles.link}>
-          read more
-        </a>
+        <div>more like this</div>
       </div>
     </div>
   );
