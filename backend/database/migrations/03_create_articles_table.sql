26696fd6af77c9c1d10c29142d1728d432506785
Said Oudouane <saidoudouane@gmail.com>
2022-06-22 11:29:19 +0100

    remove created_at field on articles schema

--- backend/database/migrations/03_create_articles_table.sql
+++ backend/database/migrations/03_create_articles_table.sql
@@ -6,8 +6,6 @@ create table if not exists articles (
     content longtext,
     preview varchar(255),
     authorId int,
-
-    created_at datetime default now(),
     foreign key (authorId) references users(id) on delete cascade on update cascade,
     foreign key (sectionId) references sections(id) on delete set null on update cascade
 
